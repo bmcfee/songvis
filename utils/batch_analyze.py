84562e1d3e39e2cd02d17ffa0e2af65ea04480a9
Brian McFee <brm2132@columbia.edu>
2013-08-08 12:11:32 -0400

    updatable index

--- utils/batch_analyze.py
+++ utils/batch_analyze.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''Usage: ./batch_analyzer output_directory/ /path/to/file/glob/*.audio'''
+'''Usage: ./batch_analyzer index.json output_directory/ /path/to/file/glob/*.audio'''
 
 
 import sys
@@ -11,11 +11,19 @@ import analyzer
 
 
 if __name__ == '__main__':
-    output_directory    = os.path.realpath(sys.argv[1])
-    files               = sorted(glob.glob(sys.argv[2]))
+    index_file          = sys.argv[1]
+    output_directory    = os.path.realpath(sys.argv[2])
+    files               = sorted(glob.glob(sys.argv[3]))
 
-    index = {}
-    for (i, f) in enumerate(files):
+    if os.path.exists(index_file):
+        with open(index_file, 'r') as f_idx:
+            index = json.load(f_idx)
+        start_index = 1 + max(map(int, index.keys()))
+    else:
+        index = {}
+        start_index = 0
+
+    for (i, f) in enumerate(files, start_index):
         print '%6d:\t%s' % (i, f)
         data = analyzer.analyze_file(f)
         output = output_directory + os.path.sep + ('%06d.json' % i)
@@ -26,5 +34,5 @@ if __name__ == '__main__':
                                 'meta': data['metadata'], 
                                 'audio': os.path.basename(f)}
 
-    with open('index.json', 'w') as f_idx:
+    with open(index_file, 'w') as f_idx:
         json.dump(index, f_idx)
